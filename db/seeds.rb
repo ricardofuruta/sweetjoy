ea0ff1d9e833f98700cf7266b7cd9b74b6cb989d
Ricardo Furuta <ricardofuruta@yahoo.com.br>
2016-04-06 16:40:41 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,13 +15,13 @@ baker1 = Baker.create([
    address: 'Rue de la Collégiale 27, Bruxelles',
    photo: open('http://res.cloudinary.com/ricardofuruta/image/upload/c_scale,h_300,w_300/v1459806781/IMG_3854_krmace.jpg')
    }])
-baker2 = Baker.create([
- { firstname: 'Binita',
-   lastname: 'Ramla',
-   description: 'I give up a carreer in software development to follow my real passion, making amazing cakes',
-   address: 'Rue du Marché aux Herbes 28, Bruxelles',
-   photo: open('http://res.cloudinary.com/ricardofuruta/image/upload/c_scale,w_300/v1459851289/binita_msq6x0.jpg')
-   }])
+# baker2 = Baker.create([
+#  { firstname: 'Binita',
+#    lastname: 'Ramla',
+#    description: 'I give up a carreer in software development to follow my real passion, making amazing cakes',
+#    address: 'Rue du Marché aux Herbes 28, Bruxelles',
+#    photo: open('http://res.cloudinary.com/ricardofuruta/image/upload/c_scale,w_300/v1459851289/binita_msq6x0.jpg')
+#    }])
 baker3 = Baker.create([
  { firstname: 'Ricardo',
    lastname: 'Furuta',
@@ -32,6 +32,6 @@ baker3 = Baker.create([
 
 
 products = Product.create([{name:'Kit Kat Cake', description: 'Cake made with Kit Kat, M&Ms, brigadeiro and chocolate', price: 100.00, baker: baker1.first, photo: open('http://res.cloudinary.com/ricardofuruta/image/upload/c_crop,h_600,w_600,x_311/v1459806782/Sweet_Joy_077_web_bq9acr.jpg')},
-  {name:'Chocolate Cake', description: 'Cake with chocolate topping, with chocolate dought and filled with brigadeiro', price: 80.00, baker: baker2.first, photo: open('http://res.cloudinary.com/ricardofuruta/image/upload/c_crop,h_600,w_600/v1459806781/Sweet_Joy_100_web_1_tjz95m.jpg')},
+  {name:'Chocolate Cake', description: 'Cake with chocolate topping, with chocolate dought and filled with brigadeiro', price: 80.00, baker: baker1.first, photo: open('http://res.cloudinary.com/ricardofuruta/image/upload/c_crop,h_600,w_600/v1459806781/Sweet_Joy_100_web_1_tjz95m.jpg')},
   {name:'Colored Cake', description: 'White Chocolate topping, with vanilla dought and filled Dulce de Leche.', price: 70.00, baker: baker3.first, photo: open('http://res.cloudinary.com/ricardofuruta/image/upload/c_crop,h_600,w_600/v1459806786/bolo_colorido_t66ldf.jpg')}])
 
