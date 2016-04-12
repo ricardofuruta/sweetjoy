56c7db104959a879e00e7cbac957a8f008eca531
binita ramla <binita.ramla@gmail.com>
2016-04-12 09:05:52 +0200

    adjust

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,4 @@
+
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
   skip_before_action :authenticate_user!, only: [:index, :show]
