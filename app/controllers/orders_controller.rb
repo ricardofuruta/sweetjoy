8d7f84074a42b28b54ed74a3fc89b7a09a4f69ff
Ricardo Furuta <ricardofuruta@yahoo.com.br>
2016-04-14 10:55:43 +0200

    checkout

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,7 @@
 class OrdersController < ApplicationController
-  before_action :set_order, only: [:show, :edit, :update, :destroy, :payment]
+  before_action :set_order, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
 
-  before_action :payment, only:[:update]
   # GET /orders
   # GET /orders.json
   def index
@@ -30,7 +29,8 @@ class OrdersController < ApplicationController
   # POST /orders
   # POST /orders.json
   def create
-    @order = current_user.order.new(order_params)
+    @order = current_user.order.new(order_pcreation_arams)
+
     if @order.save
       redirect_to @order, notice: 'Order was successfully created.'
     else
@@ -41,15 +41,23 @@ class OrdersController < ApplicationController
   # PATCH/PUT /orders/1
   # PATCH/PUT /orders/1.json
   def update
-    respond_to do |format|
-      if @order.update(status: "PAID")
-        format.html { redirect_to @order, notice: 'Order was successfully updated.' }
-        format.json { render :show, status: :ok, location: @order }
-      else
-        format.html { render :edit }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
+    if @order.update(order_update_params)
+      redirect_to new_order_payment_path(@order)
+    else
+      flash[:alert] = "Unable to proceed to checkout"
+      render :show
     end
+
+
+    # respond_to do |format|
+    #   if @order.update(status: "PAID")
+    #     format.html { redirect_to @order, notice: 'Order was successfully updated.' }
+    #     format.json { render :show, status: :ok, location: @order }
+    #   else
+    #     format.html { render :edit }
+    #     format.json { render json: @order.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # DELETE /orders/1
@@ -64,13 +72,22 @@ class OrdersController < ApplicationController
 
   private
 
-    # Use callbacks to share common setup or constraints between actions.
-    def set_order
-      @order = Order.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def order_params
-      params.require(:order).permit(:delivery_address, :delivery_date)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_order
+    @order = Order.find(params[:id])
   end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def order_creation_params
+    params.require(:order).permit(:delivery_address, :delivery_date)
+  end
+
+  def order_update_params
+    params.require(:order).permit(
+      :fullname,
+      :delivery_address,
+      :delivery_date,
+      :instructions
+    )
+  end
+end
